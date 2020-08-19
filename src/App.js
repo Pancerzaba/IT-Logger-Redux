31d947fe07ce7fb7233ced684a6f3e78162c426f
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-08-19 23:46:35 +0200

    SearchBar and Logs Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
+import SearchBar from './components/layout/SearchNavbar';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -9,9 +10,10 @@ const App=()=> {
     //Init Materialize JS
     M.AutoInit();
   })
-  return <div className="App">
+  return <Fragment className="App">
+    <SearchBar/>
      My App
-    </div>
+    </Fragment>
 };
 
 export default App;
